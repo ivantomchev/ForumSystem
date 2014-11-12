70c3312ad1a44a5e0aab51fd3dc683fc3372d06b
ivantomchev <ivantomchev@gmail.com>
2014-11-12 18:19:46 +0200

    Implemented automatic CreatedOn,DeletedOn,ModifiedOn datafields Added Model base classes and interfaces (IAuditInfo,IDeletableEntity)

--- Source/Data/ForumSystem.Data.Models/ApplicationUser.cs
+++ Source/Data/ForumSystem.Data.Models/ApplicationUser.cs
@@ -1,12 +1,19 @@
 ﻿namespace ForumSystem.Data.Models
 {
+    using ForumSystem.Data.Common.Models;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
     using System.Security.Claims;
     using System.Threading.Tasks;
 
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser,IAuditInfo,IDeletableEntity
     {
+        public ApplicationUser()
+        {
+            this.CreatedOn = DateTime.Now;
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -14,5 +21,16 @@
             // Add custom user claims here
             return userIdentity;
         }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool PreserveCreatedOn { get; set; }
+
+        public DateTime? ModifiedOn{ get; set; }
+
+        public bool IsDeleted{ get; set; }
+
+        public DateTime? DeletedOn{ get; set; }
+
     }
 }
